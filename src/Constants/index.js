fbd8b417b7d17b8a5ab057e468dde01d3255536a
priya singh <ps9986219@gmail.com>
2025-03-28 20:22:20 +0530

    first commit

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -26,15 +26,15 @@ export const navLinks = [
 export const clientReviews = [
     {
         id: 1,
-        name: 'Emily Johnson',
-        position: 'Marketing Director at GreenLeaf',
+        name: 'tanisha',
+        position: 'CEO of modinfra',
         img: 'assets/review1.png',
         review:
             'Priya’s coding skills are top-notch! She built a sleek, fast, and user-friendly website for our business. Super professional and easy to work with!'
     },
     {
         id: 2,
-        name: 'Mark Rogers',
+        name: 'Mark',
         position: 'Founder of TechGear Shop',
         img: 'assets/review2.png',
         review:
@@ -51,53 +51,16 @@ export const clientReviews = [
     },
     {
         id: 4,
-        name: 'Ether Smith',
-        position: 'CEO of BrightStar Enterprises',
+        name: 'Ethan',
+        position: 'CEO of BrightStar Tech',
         img: 'assets/review4.png',
         review:
-            'Priya was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. Her skills in both frontend backend dev are top-notch.',
+            'Priya was a pleasure to work with. she understood our requirements perfectly and delivered a website that exceeded our expectations. Her skills in both frontend backend dev are top-notch.',
     },
 ];
 
 export const myProjects = [
-    {
-        title: 'GiftAura - Personalized Gifting Platform',
-        desc: 'GiftAura is an innovative e-commerce platform that specializes in personalized gifting solutions. With AI-driven customization features, users can create unique gifts tailored to their loved ones effortlessly.',
-        subdesc:
-            'Developed as a cutting-edge Software-as-a-Service platform using Next.js 14, Tailwind CSS, TypeScript, Framer Motion, and Convex, GiftAura ensures a seamless and engaging shopping experience.',
-        href: 'https://www.youtube.com/watch?v=your_video_link',
-        texture: '/textures/project/project2.mp4',
-        logo: '/assets/4.png',
-        logoStyle: {
-            backgroundColor: '#1E1A3B',
-            border: '0.2px solid #2A2555',
-            boxShadow: '0px 0px 60px 0px #6A5ACD4D',
-        },
-        spotlight: '/assets/spotlight2.png',
-        tags: [
-            {
-                id: 1,
-                name: 'React.js',
-                path: '/assets/react.svg',
-            },
-            {
-                id: 2,
-                name: 'TailwindCSS',
-                path: '/assets/tailwindcss.png',
-            },
-            // {
-            //     id: 3,
-            //     name: 'TypeScript',
-            //     path: '/assets/typescript.png',
-            // },
-            {
-                id: 4,
-                name: 'Framer Motion',
-                path: '/assets/framer.png',
-            },
-        ],
-    }
-,
+   
     {
     "title": "BookStore - Your Gateway to Knowledge",
     "desc": "An interactive platform designed for book lovers and learners. Explore a vast collection of educational resources, courses, and books to enhance your knowledge every day.",
@@ -168,14 +131,17 @@ texture: '/textures/project/bookprojects.mp4',
 
         ],
     },
+    
+
     {
-        title: 'Horizon - Online Banking Platform',
-        desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
-        subdesc:
-            'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
-        href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
-        texture: '/textures/project/project4.mp4',
-        logo: '/assets/project-logo4.png',
+        "title": "Trendify - Your One-Stop E-Commerce Destination",
+
+       "desc": "Trendify is a cutting-edge e-commerce platform designed to offer a seamless shopping experience. Browse top trending products, enjoy secure payments, and get fast deliveries—all in one place.",
+        
+        "subdesc":"Built with React, Next.js, and Stripe for secure transactions, Trendify ensures a smooth, fast, and user-friendly shopping experience for modern consumers." ,
+        href: 'https://mern-ecommerce-lake-one.vercel.app/',
+        texture: '/textures/project/project3 (2).mp4',
+        logo: '/assets/project-logo3.png',
         logoStyle: {
             backgroundColor: '#0E1F38',
             border: '0.2px solid #0E2D58',
@@ -206,19 +172,19 @@ texture: '/textures/project/bookprojects.mp4',
         ],
     },
     {
-        title: 'Imaginify - AI Photo Manipulation App',
-        desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
+        title: 'GiftAura - Personalized Gifting Platform',
+        desc: 'GiftAura is an innovative e-commerce platform that specializes in personalized gifting solutions. With AI-driven customization features, users can create unique gifts tailored to their loved ones effortlessly.',
         subdesc:
-            'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
-
-        texture: '/textures/project/project5.mp4',
-        logo: '/assets/project-logo5.png',
+            'Developed as a cutting-edge Software-as-a-Service platform using Next.js 14, Tailwind CSS, TypeScript, Framer Motion, and Convex, GiftAura ensures a seamless and engaging shopping experience.',
+        href: 'https://giftaura.vercel.app/',
+        texture: '/textures/project/project3.mp',
+        logo: '/assets/4.png',
         logoStyle: {
-            backgroundColor: '#1C1A43',
-            border: '0.2px solid #252262',
-            boxShadow: '0px 0px 60px 0px #635BFF4D',
+            backgroundColor: '#1E1A3B',
+            border: '0.2px solid #2A2555',
+            boxShadow: '0px 0px 60px 0px #6A5ACD4D',
         },
-        spotlight: '/assets/spotlight5.png',
+        spotlight: '/assets/spotlight2.png',
         tags: [
             {
                 id: 1,
@@ -228,20 +194,23 @@ texture: '/textures/project/bookprojects.mp4',
             {
                 id: 2,
                 name: 'TailwindCSS',
-                path: 'assets/tailwindcss.png',
+                path: '/assets/tailwindcss.png',
             },
-            {
-                id: 3,
-                name: 'TypeScript',
-                path: '/assets/typescript.png',
-            },
+            // {
+            //     id: 3,
+            //     name: 'TypeScript',
+            //     path: '/assets/typescript.png',
+            // },
             {
                 id: 4,
                 name: 'Framer Motion',
                 path: '/assets/framer.png',
             },
         ],
-    },
+    } ,
+            
+        
+
 ];
 
 export const calculateSizes = (isSmall, isMobile, isTablet) => {
@@ -260,7 +229,7 @@ export const workExperiences = [
         id: 1,
         name: 'Frontend Developer',
         pos: 'Heeric technologies llp',
-        duration: ' nov 2024 - Present',
+        duration: ' jan 2023 - Present',
         title: "As a Frontend Developer, I specialize in crafting dynamic and responsive user interfaces. My expertise includes building scalable web applications, optimizing performance, and ensuring seamless user experiences.",
         icon: '/assets/framer.svg',
         animation: 'victory',
@@ -269,7 +238,7 @@ export const workExperiences = [
         id: 2,
         name: 'Web Developer (Freelancer)',
         pos: 'Self-Employed',
-        duration: 'jan 2024 - dec 2024',
+        duration: 'part time',
         title: "As a Freelance Web Developer, I design and develop custom websites, ensuring high performance, responsive design, and seamless user experience. I specialize in front-end and back-end technologies to build scalable web solutions for diverse clients.",
         icon: '/assets/figma.svg',
         animation: 'victory',
